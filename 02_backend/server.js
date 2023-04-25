4c1d5083fb6e7ec2e9e4c4e6cc80b05a73f807ef
kamesh singh <88535029+kamesh7773@users.noreply.github.com>
2023-04-25 23:24:22 +0530

    backend push

---
+++ 02_backend/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const Cors = require('cors');
+require("dotenv").config();
+const dbConfig = require("./config/dbConfig");
+app.use(express.json());
+const userRoute = require("./routes/userRoute");
+const adminRoute = require("./routes/adminRoute");
+const doctorRoute = require("./routes/doctorsRoute");
+const testsRoute = require("./routes/testsRoute");
+
+// routes
+app.use(Cors({
+    origin: '*'
+}))
+app.use("/api/user", userRoute);
+app.use("/api/admin", adminRoute);
+app.use("/api/doctor", doctorRoute);
+app.use("/api/tests", testsRoute);
+
+// PORT
+const port = process.env.PORT || 5000;
+
+app.get("/", (req, res) => res.send("Hello World!"));
+app.listen(port, () => console.log(`Node Express Server Started at ${port}!`));
